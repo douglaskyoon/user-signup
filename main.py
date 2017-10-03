f98a6140975cc092e2861d967df42728a7b3f8e7
Doug Yoon <douglaskyoon@gmail.com>
2017-10-03 16:35:17 -0500

    finished user signup

---
+++ main.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+
+
+@app.route("/welcome", methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+    username_error=''
+    password_error=''
+    verify_error=''
+    email_error=''
+
+    if len(username) < 3 or len(username) > 20:
+        username_error="that is not a valid username"
+        
+
+    if len(password) < 3 or len(password) > 20:
+        password_error="that is not a valid password"
+        
+
+    if password != verify:
+        verify_error = "password does not match"
+
+    count_1 = 0
+    count_2 = 0
+    for x in email:
+        if ord(x) == 64:
+            count_1 += 1
+        if ord(x) == 46:
+            count_2 += 1
+    if count_1 != 1 or count_2 != 1:
+        email_error = "that is not a valid email"
+
+    if not username_error and not password_error and not verify_error and not email:
+
+        return render_template('welcome.html', username=username)
+
+    elif not username_error and not password_error and not verify_error and not email_error:
+
+        return render_template('welcome.html', username=username)
+
+    else:
+        return render_template('signup.html', 
+            username_error=username_error, 
+            password_error=password_error, 
+            verify_error=verify_error,
+            email_error=email_error,
+            email_input=email,
+            username_input=username)
+
+
+
+@app.route("/")
+def index():
+    return render_template('signup.html')
+
+app.run()
